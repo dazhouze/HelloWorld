e5e1f87350b5f7c2b844a0ebaa9c425ab78141b8
dazhouze <dazhouze@163.com>
2016-08-04 11:35:20 +0800

    Class initialization. Function in Class must need self paramter.

---
+++ practice_12_oop.py
@@ -0,0 +1,22 @@
+class Student (object):
+
+    def __init__ (self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score (self):
+        print ('%s %s'% (self.name, self.score))
+
+    def his_pic (self):
+        print ('''
+                ___      _      ___    _____
+               |___|    /_\    |___|     |
+               |___|   /   \   |   \     |
+
+              ''')
+
+bart = Student('Bart Simpson', 59)
+print(bart.name)
+print(bart.score)
+bart.print_score()
+bart.his_pic()
