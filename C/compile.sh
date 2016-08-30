9a9cdbd65209089b324ead1af259087666a27364
dazhouze <dazhouze@gmail.com>
2016-08-30 11:36:02 +0800

    Add start end hint.

--- C/compile.sh
+++ C/compile.sh
@@ -2,7 +2,9 @@
 
 if [[ -n "$1" ]]; then
     gcc -O -Wall -W -pedantic -ansi -std=c99 -o test $1
+    echo "===== STAERT ====="
     ./test
+    echo "===== END ====="
 else
     echo "Argument error!\n"
 fi
