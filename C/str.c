aa211f6f2bb4175893129cf177323479cd3ed5ba
dazhouze <dazhouze@gmail.com>
2017-01-13 10:06:32 +0800

    str

---
+++ C/str.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() 
+{
+    char str1[] = "Hello1";
+    char *str2 = "Hello2";
+    char str3[10];
+    printf("str1: %s\n", str1);
+    printf("str2: %s\n", str2);
+    printf("str3: %s\n", str3);
+}
