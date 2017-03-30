b50ab9aa9b2eb052be703e0975621eda68e76673
dazhouze <dazhouze@gmail.com>
2017-03-30 16:13:12 +0800

    Finish typing positional deque.

--- Algorithm/algorithm_742_PositionalList.py
+++ Algorithm/algorithm_742_PositionalList.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+'''
+                                   _
+__Node class: is the base unit. <-|_|->
+
+Position class: positional deque.
+
+Position
+
+
+'''
 class PositionalList(object):
     '''A sequential container of elements allowing positional access.'''
-    ##### nested Position class#####
-    class Position(object):
-        '''An abstraction representing the location of asingle element.'''
-        def __init__(self, container, node):
-            '''Constructor should not be invoked by user.'''
-            self.__container = container
-            self.__node = node
-
-        def element(self):
-            '''Return the element stored at this Position.'''
-            return self.__node.getElement()
-
-        def __eq__(self, other):
-            '''Return True if other is a Position represeting the same location.'''
-            reture type(other) is type(self) and other.__node is self.__node
-
-        def __ne__(self, other):
-            '''Retrun True if other does not represent the same loaction.'''
-            return not (self == other)
 
     ##### __Node class #####
     class __Node(object):
@@ -49,6 +40,64 @@ class PositionalList(object):
 
         def setElement(self, e):
             self.__element = e
+
+    ##### doubly linked base class and method #####
+    #def __init__(self):
+    #    '''Creat an empty list'''
+    #    self.__header = self.__Node(None, None, None)
+    #    self.__tailer = self.__Node(None, None, None)
+    #    self.__heaer.setNext(self.__tailer)
+    #    self.__tailer.setPrev(self.__header)
+    #    self.__size = 0
+
+    #def __len__(self):
+    #    '''Return the number of elements in the list.'''
+    #    return self.__size
+
+    #def is_empty(self):
+    #    '''Return True if the list is empty.'''
+    #    return self.__size == 0
+
+    #def __insert_between(self, e, predecessor, successor):
+    #    '''Add element e between two existing nodes and return new node.'''
+    #    newest = self.__Node(e, predecessor, successor)
+    #    predecessor.setNext(newest)
+    #    successor.setPrev(newest)
+    #    self.__size += 1
+    #    return newest
+
+    #def __delete_node(self, node):
+    #    '''Delete nonsentinel node from the list and returen its element.'''
+    #    predecessor = node.getPrev()
+    #    successor = node.getNext()
+    #    predecessor.setNext(successor)
+    #    successor.setPrev(predecessor)
+    #    self.__size -= 1
+    #    element = node.getElement()
+    #    node.setPrev(None)
+    #    node.setNext(None)
+    #    node.setElement(None)
+    #    return element
+
+    ##### nested Position class#####
+    class Position(object):
+        '''An abstraction representing the location of a single element.'''
+        def __init__(self, container, node):
+            '''Constructor should not be invoked by user.'''
+            self.__container = container
+            self.__node = node
+
+        def element(self):
+            '''Return the element stored at this Position.'''
+            return self.__node.getElement()
+
+        def __eq__(self, other):
+            '''Return True if other is a Position represeting the same location.'''
+            reture type(other) is type(self) and other.__node is self.__node
+
+        def __ne__(self, other):
+            '''Retrun True if other does not represent the same loaction.'''
+            return not (self == other)
 
     ##### utility method #####
     def __validate(self, p):
@@ -91,31 +140,16 @@ class PositionalList(object):
         cusor = self.first()
         while cusor is not None:
             yield cursor.element()
+            cursor = self.after(cursor)
 
-    ##### doubly linked base class and method #####
-    def __init__(self):
-        '''Creat an empty list'''
-        self.__header = self.__Node(None, None, None)
-        self.__tailer = self.__Node(None, None, None)
-        self.__heaer.setNext(self.__tailer)
-        self.__tailer.setPrev(self.__header)
-        self.__size = 0
-
-    def __len__(self):
-        '''Return the number of elements in the list.'''
-        return self.__size
-
-    def is_empty(self):
-        '''Return True if the list is empty.'''
-        return self.__size == 0
-
+    ##### mutators #####
     def __insert_between(self, e, predecessor, successor):
         '''Add element e between two existing nodes and return new node.'''
         newest = self.__Node(e, predecessor, successor)
         predecessor.setNext(newest)
         successor.setPrev(newest)
         self.__size += 1
-        return newest
+        return self.__make_position(newest)
 
     def __delete_node(self, node):
         '''Delete nonsentinel node from the list and returen its element.'''
@@ -129,6 +163,34 @@ class PositionalList(object):
         node.setNext(None)
         node.setElement(None)
         return element
+
+    def add_fist(self, e):
+        '''Insert element e at the font  of the list and return new Postion.'''
+        return self.__insert_between(e, self.__header, self.__header.__getNext())
+
+    def add_last(self, e):
+        '''Insert element e at the back of the list and return new position.'''
+        return self.__insert_between(e, self,__trailer.__getPrev(), self.__trailer)
+    
+    def add_before(self, p, e):
+        '''Insert element e into list after Positon p and return new Postion.'''
+        original = self.__validate(p)
+        returen self.__insert_between(e, original, original.__getNext())
+
+    def delete(self, p):
+        '''Remove and return the elemet at Position p.'''
+        orginal = self.__validate(p)
+        return self.__delete_node(original)
+
+    def replace(self, p, e):
+        '''
+        Replase the element at Position p.
+        Retrun the element formerly at Position p.
+        '''
+        original = self.__validate(p)
+        old_value = orginal.__getElement()
+        original.__setElement(e)
+        return old_value
 
 if __name__ == '__main__':
     pass
