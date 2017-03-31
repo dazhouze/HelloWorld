8f07da962342d815577354a1f49269b6a2b12b68
dazhouze <dazhouze@gmail.com>
2017-03-31 15:39:57 +0800

    Debug and figure out the details.

--- Algorithm/algorithm_742_PositionalList.py
+++ Algorithm/algorithm_742_PositionalList.py
@@ -1,17 +1,87 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
-                                   _
-__Node class: is the base unit. <-|_|->
+Position class: 
+    __init__(): 
+        __container: a ref to a instance of the Positional list   _      _      _      _ 
+                                                               <-|_|-><-|_|-><-|_|-><-|_|->   
+        __node: a ref to a intance of __Node class    _    
+                                                   <-|_|->  
+    __eq__() __ne__()                      
+    element()
+    __make_position()
+    __validate()
 
-Position class: positional deque.
+__Node class: is the base unit.   
+       _
+    <-|_|->
+         __init__()
+         getPrev()    setPrev()
+         getNext()    setNext()
+         getElement() setElement()
 
-Position
-
-
+PositionalList class: positional deque.
+       _      _      _      _ 
+    <-|_|-><-|_|-><-|_|-><-|_|->
+    basic func:
+        __init__()
+        __len__()
+        __iter__()
+        is_empty()
+        first()    last()
+        before(p)  after(p)
+    insert + delete func:
+        __insert_between()   __delete_between()
+        add_first()          add_last()
+        add_before()         add_after()
+        delet_()
+        replace()
 '''
 class PositionalList(object):
     '''A sequential container of elements allowing positional access.'''
+
+    ##### Position class#####
+    class Position(object):
+        '''An abstraction representing the location of a single element.'''
+        def __init__(self, container, node):
+            '''Constructor should not be invoked by user.'''
+            self.__container = container # instance of PositionList class
+            self.__node = node # instance of __Node class
+            
+        def getContainer(self):
+            return self.__container
+
+        def getNode(self):
+            return self.__node
+
+        def element(self):
+            '''Return the element stored at this Position.'''
+            return self.getNode().getElement()
+
+        def __eq__(self, other):
+            '''Return True if other is a Position represeting the same location.'''
+            return type(other) is type(self) and other.getNode() is self.getNode()
+
+        def __ne__(self, other):
+            '''Retrun True if other does not represent the same loaction.'''
+            return not (self == other)
+
+    ##### utility method #####
+    def __validate(self, p):
+        '''Return position's node, or raise approprate error if invalid.'''
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Position type')
+        if p.getContainer() is not self:
+            raise ValueError('p does not belong to this container')
+        if p.getNode().getNext() is None:
+            raise ValueError('p is no longer valid')
+        return p.getNode()
+
+    def __make_position(self, node):
+        '''Return Position instance for given node (or None if sentinel).'''
+        if node is self.__header or node is self.__trailer:
+            return None
+        return self.Position(self, node)
 
     ##### __Node class #####
     class __Node(object):
@@ -41,80 +111,23 @@ class PositionalList(object):
         def setElement(self, e):
             self.__element = e
 
-    ##### doubly linked base class and method #####
-    #def __init__(self):
-    #    '''Creat an empty list'''
-    #    self.__header = self.__Node(None, None, None)
-    #    self.__tailer = self.__Node(None, None, None)
-    #    self.__heaer.setNext(self.__tailer)
-    #    self.__tailer.setPrev(self.__header)
-    #    self.__size = 0
+    ##### Positional list class #####
+    def __init__(self):
+        '''Creat an empty list'''
+        self.__header = self.__Node(None, None, None)
+        self.__trailer = self.__Node(None, None, None)
+        self.__header.setNext(self.__trailer)
+        self.__trailer.setPrev(self.__header)
+        self.__size = 0
 
-    #def __len__(self):
-    #    '''Return the number of elements in the list.'''
-    #    return self.__size
+    def __len__(self):
+        '''Return the number of elements in the list.'''
+        return self.__size
 
-    #def is_empty(self):
-    #    '''Return True if the list is empty.'''
-    #    return self.__size == 0
+    def is_empty(self):
+        '''Return True if the list is empty.'''
+        return self.__size == 0
 
-    #def __insert_between(self, e, predecessor, successor):
-    #    '''Add element e between two existing nodes and return new node.'''
-    #    newest = self.__Node(e, predecessor, successor)
-    #    predecessor.setNext(newest)
-    #    successor.setPrev(newest)
-    #    self.__size += 1
-    #    return newest
-
-    #def __delete_node(self, node):
-    #    '''Delete nonsentinel node from the list and returen its element.'''
-    #    predecessor = node.getPrev()
-    #    successor = node.getNext()
-    #    predecessor.setNext(successor)
-    #    successor.setPrev(predecessor)
-    #    self.__size -= 1
-    #    element = node.getElement()
-    #    node.setPrev(None)
-    #    node.setNext(None)
-    #    node.setElement(None)
-    #    return element
-
-    ##### nested Position class#####
-    class Position(object):
-        '''An abstraction representing the location of a single element.'''
-        def __init__(self, container, node):
-            '''Constructor should not be invoked by user.'''
-            self.__container = container
-            self.__node = node
-
-        def element(self):
-            '''Return the element stored at this Position.'''
-            return self.__node.getElement()
-
-        def __eq__(self, other):
-            '''Return True if other is a Position represeting the same location.'''
-            reture type(other) is type(self) and other.__node is self.__node
-
-        def __ne__(self, other):
-            '''Retrun True if other does not represent the same loaction.'''
-            return not (self == other)
-
-    ##### utility method #####
-    def __validate(self, p):
-        '''Return position's node, or raise approprate error if invalid.'''
-        if not isinstance(p, self.Position):
-            raise TypeError('p must be proper Position type')
-        if p.__container is not self:
-            raise ValueError('p does not belong to this container')
-        if p.__node.getNext() is None:
-            raise ValueError('p is no longer valid')
-        return p.__node
-
-    def __make_position(self, node):
-        '''Return Position instance for given node (or None if sentinel).'''
-        if node is self.__header or node is self.__trailer:
-            return None
-        return self.Position(self, node)
 
     ##### accessors #####
     def first(self):
@@ -137,8 +150,8 @@ class PositionalList(object):
 
     def __iter__(self):
         '''Generatea forward iteration of the elements of the list.'''
-        cusor = self.first()
-        while cusor is not None:
+        cursor = self.first()
+        while cursor is not None:
             yield cursor.element()
             cursor = self.after(cursor)
 
@@ -164,18 +177,23 @@ class PositionalList(object):
         node.setElement(None)
         return element
 
-    def add_fist(self, e):
+    def add_first(self, e):
         '''Insert element e at the font  of the list and return new Postion.'''
-        return self.__insert_between(e, self.__header, self.__header.__getNext())
+        return self.__insert_between(e, self.__header, self.__header.getNext())
 
     def add_last(self, e):
         '''Insert element e at the back of the list and return new position.'''
-        return self.__insert_between(e, self,__trailer.__getPrev(), self.__trailer)
+        return self.__insert_between(e, self.__trailer.getPrev(), self.__trailer)
     
     def add_before(self, p, e):
         '''Insert element e into list after Positon p and return new Postion.'''
         original = self.__validate(p)
-        returen self.__insert_between(e, original, original.__getNext())
+        return self.__insert_between(e, original, original.getNext())
+
+    def add_after(self, p, e):
+        '''Insert element e into list after Position pand return new Position.'''
+        original = self.__validate(p)
+        return self.__insert_between(e, original, original.getNext())
 
     def delete(self, p):
         '''Remove and return the elemet at Position p.'''
@@ -188,9 +206,22 @@ class PositionalList(object):
         Retrun the element formerly at Position p.
         '''
         original = self.__validate(p)
-        old_value = orginal.__getElement()
-        original.__setElement(e)
+        old_value = orginal.getElement()
+        original.setElement(e)
         return old_value
 
 if __name__ == '__main__':
-    pass
+    PL = PositionalList()
+    p1 = PL.add_first('H')
+    p2 = PL.add_after(p1, 'E')
+    p5 = PL.add_last('O')
+    p4 = PL.add_before(p5, 'L')
+    p3 = PL.add_before(p4, 'L')
+    p = PL.last()
+    for x in ' WORLD':
+        p = PL.add_after(p, x)
+    print('length of PositionalList:%d'%len(PL))
+    print(PL.first().getNode().getElement())
+    for x in PL:
+        print(x, end = ' ')
+    print('')
