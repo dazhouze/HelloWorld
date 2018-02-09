5ab50d1f1b7eddd0e9ffcb7d735d901dfbfcb85c
dazhouze <dazhouze@link.cuhk.edu.hk>
2018-02-09 21:54:55 +0800

    update.

--- Machine_learning/ML_66_nested_cross_valid.py
+++ Machine_learning/ML_66_nested_cross_valid.py
@@ -67,3 +67,35 @@ if __name__ == '__main__':
 	print('Precision: %.3f' % precision_score(y_true=y_test, y_pred=y_pred))
 	print('Recall: %.3f' % recall_score(y_true=y_test, y_pred=y_pred))
 	print('F1: %.3f' % f1_score(y_true=y_test, y_pred=y_pred))
+
+	# ROC AUC
+	from sklearn.metrics import roc_curve, auc
+	from scipy import interp
+	pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2), LogisticRegression(penalty='l2', random_state=1, C=1000))
+	X_train2 = X_train[:, [4, 14]]
+	from sklearn.model_selection import StratifiedKFold
+	cv = list(StratifiedKFold(n_splits=3, random_state=1).split(X_train, y_train))
+	fig = plt.figure(figsize=(7, 5))
+	mean_tpr = 0
+	mean_fpr = np.linspace(0, 1, 100)
+	all_tpr = []
+	for i,(train, test) in enumerate(cv):
+		probas = pipe_lr.fit(X_train2[train], y_train[train]).predict_proba(X_train2[test])
+		fpr, tpr, thresholds = roc_curve(y_train[test], probas[:, 1], pos_label=1)
+		mean_tpr += interp(mean_fpr, fpr, tpr)
+		mean_tpr[0] = 0.0
+		roc_auc = auc(fpr, tpr)
+		plt.plot(fpr, tpr, label='ROC fold %d (area =  %.2f)' % (i+1, roc_auc))
+	plt.plot([0, 1], [0, 1], linestyle='--', color=(0.6,0.6,0.6), label='random guessing')
+	mean_tpr /= len(cv)
+	mean_tpr[-1] = 1
+	mean_auc = auc(mean_fpr, mean_tpr)
+	plt.plot(mean_fpr, mean_tpr, 'k--', label='mean ROC (area = %0.2f)' % mean_auc, lw=2)
+	plt.plot([0, 0, 1], [0, 1, 1], linestyle=':', color='black', label='perfect performance')
+	plt.xlim([-0.05, 1.05])
+	plt.ylim([-0.05, 1.05])
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+	plt.legend(loc='lower right')
+	fig.savefig('ROC.pdf')
+
