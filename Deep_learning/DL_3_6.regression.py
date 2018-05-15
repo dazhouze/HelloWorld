30b599a5a3d42450fb10a69f9a6ba3c39b2edf11
Zhou Ze <dazhouze@link.cuhk.edu.hk>
2018-05-15 14:43:47 +0800

    update.

--- Deep_learning/DL_3_6.regression.py
+++ Deep_learning/DL_3_6.regression.py
@@ -11,83 +11,79 @@ import numpy as np
 import tensorflow as tf
 import keras
 
-def vectorize_sequences(sequences, dimension=10000):
-	results = np.zeros((len(sequences), dimension))
-	for i, sequence in enumerate(sequences):
-		results[i, sequence] = 1.
-	return results
-
-def to_one_hot(labels, dimension=46):
-	results = np.zeros((len(labels), dimension))
-	for i, label in enumerate(labels):
-		results[i, label] = 1.
-	return results
-
-if __name__ == '__main__':
-	from keras.datasets import reuters
-	(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
-	word_index = reuters.get_word_index()
-	reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-	decoded_newswire = ' '.join([reverse_word_index.get(i-3, '?') for i in train_data[0]])
-	print(decoded_newswire)
-
-	x_train = vectorize_sequences(train_data)
-	x_test = vectorize_sequences(test_data)
-	one_hot_train_labels = to_one_hot(train_labels)
-	one_hot_test_labels = to_one_hot(test_labels)
-	from keras.utils.np_utils import to_categorical
-	one_hot_train_labels = to_categorical(train_labels)
-	one_hot_test_labels = to_categorical(test_labels)
-
+def build_model():
 	from keras import models
 	from keras import layers
 	model = models.Sequential()
-	model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+	model.add(layers.Dense(64, activation='relu', input_shape=(train_data.shape[1],)))
 	model.add(layers.Dense(64, activation='relu'))
-	model.add(layers.Dense(46, activation='softmax'))
-	model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-	x_val = x_train[:1000]
-	partial_x_train = x_train[1000:]
-	y_val = one_hot_train_labels[:1000]
-	partial_y_train = one_hot_train_labels[1000:]
+	model.add(layers.Dense(1))
+	model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+	return model
 
-	history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
-	model.save('./DL_3_5.h5')
-	results = model.evaluate(x_test, one_hot_test_labels)
-	print(results)
+def smooth_curve(points, factor=0.9):
+	smoothed_points = []
+	for point in points:
+		if smoothed_points:
+			previous = smoothed_points[-1]
+			smoothed_points.append(previous * factor + point*(1-factor))
+		else:
+			smoothed_points.append(point)
+	return smoothed_points
 
+if __name__ == '__main__':
+	from keras.datasets import boston_housing
+	(train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
+	print(train_data.shape)
+	print(test_data.shape)
+	mean = train_data.mean(axis=0)
+	std = train_data.std(axis=0)
+	train_data = (train_data-mean)/std
+	test_data = (test_data-mean)/std
+	
+	k = 4  # K-fold validataion
+	num_val_samples = len(train_data) // k
+	num_epochs = 500
+	all_mae_history = []
+	for i in range(k):
+		print('processing fold #', i)
+		val_data = train_data[i*num_val_samples: (i+1)*num_val_samples]
+		val_targets = train_targets[i*num_val_samples: (i+1)*num_val_samples]
+		partial_train_data = np.concatenate([train_data[:i*num_val_samples], train_data[(i+1)*num_val_samples:]], axis=0)
+		partial_train_targets = np.concatenate([train_targets[:i*num_val_samples], train_targets[(i+1)*num_val_samples:]], axis=0)
+		model = build_model()
+		history = model.fit(partial_train_data, partial_train_targets, validation_data=(val_data, val_targets), epochs=num_epochs, batch_size=1, verbose=0)
+		mae_history = history.history['val_mean_absolute_error']
+		all_mae_history.append(mae_history)
 
-	with matplotlib.backends.backend_pdf.PdfPages('DL_3_5.pdf') as pdf_all: 
-		history_dict = history.history
-		print(history_dict.keys())
-		loss_values = history_dict['loss']
-		val_loss_values = history_dict['val_loss']
-		epochs = range(1, len(history_dict['acc'])+1)
+	average_mae_history = [np.mean([x[i] for x in all_mae_history]) for i in range(num_epochs)]
 
+	with matplotlib.backends.backend_pdf.PdfPages('DL_3_6.pdf') as pdf_all: 
 		fig = plt.figure()
-		plt.plot(epochs, loss_values, 'bo', label='Training loss')  # blue dot
-		plt.plot(epochs, val_loss_values, 'b', label='Training loss')  # blue line
-		plt.title('Training and validation loss', fontsize=20)
+		plt.plot(range(1, len(average_mae_history)+1), average_mae_history, 'b')  # blue line
+		#plt.title('Training and validation loss', fontsize=20)
 		plt.xlabel('Epochs', fontsize=20)
-		plt.ylabel('Loss', fontsize=20)
+		plt.ylabel('Validation MAE', fontsize=20)
 		plt.xticks(fontsize=15)
 		plt.yticks(fontsize=15)
-		plt.legend()
+		#plt.legend()
 		plt.tight_layout()
 		pdf_all.savefig(fig)
 
-		acc_values = history_dict['acc']
-		val_acc_values = history_dict['val_acc']
 		fig = plt.figure()
-		plt.plot(epochs, acc_values, 'bo', label='Training acc')  # blue dot
-		plt.plot(epochs, val_acc_values, 'b', label='Training acc')  # blue line
-		plt.title('Training and validation loss', fontsize=20)
+		smooth_mae_history = smooth_curve(average_mae_history[10:])
+		plt.plot(range(1, len(smooth_mae_history)+1), smooth_mae_history, 'b')
+		#plt.title('Training and validation loss', fontsize=20)
 		plt.xlabel('Epochs', fontsize=20)
-		plt.ylabel('Loss', fontsize=20)
+		plt.ylabel('Validation MAE', fontsize=20)
 		plt.xticks(fontsize=15)
 		plt.yticks(fontsize=15)
-		plt.legend()
+		#plt.legend()
 		plt.tight_layout()
 		pdf_all.savefig(fig)
 
-
+	model = build_model()
+	model.fit(train_data, train_targets, epochs=80, batch_size=16, verbose=0)
+	test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
+	model.save('./DL_3_6.h5')
+	print(test_mae_score)
